0c358c31aed620e4c878f8d433f2966d7accc50c
Ethan Jolles <erjolles@gmail.com>
2022-01-09 18:14:05 -0500

    added two functions including main

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 import functions
 
 print(functions.math("+", 2.5, 2.5))
+
+print(functions.math("+", 2.6, 2.6))
--- functions.py
+++ functions.py
@@ -1,10 +1,19 @@
 def math(operation, num1, num2):
-    match operation:
-        case "+":
-            return num1 + num2
-        case "-":
-            return num1 - num2
-        case "*":
-            return num1 * num2
-        case "/":
-            return num1 / num2
+    if operation == "+":
+        answer = num1 + num2
+    elif operation == "-":
+        answer = num1 - num2
+    elif operation == "*":
+        answer = num1 * num2
+    elif operation == "/":
+        answer = num1 / num2
+    else:
+        return "ERROR, UNIDENTIFIED OPERATOR"
+    return answer_simplifier(answer)
+
+
+def answer_simplifier(answer):
+    if answer % 1 == 0:
+        return int(answer)
+    else:
+        return answer
