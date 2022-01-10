f2b58d9b26ae1e976a57c688ea52017c4708ff9c
Ethan Jolles <erjolles@gmail.com>
2022-01-09 21:02:06 -0500

    updating gui

--- gui.py
+++ gui.py
@@ -1,12 +1,31 @@
 import tkinter as tk
 
+
 # Define GUI window
 def make_calculator():
+    #Set up window
     window = tk.Tk()
-    header = tk.Label(text="Calculator")
-    text_box = tk.Entry()
+    window.title("Calculator")
+    window.resizable(width=False,height=False)
 
-    header.pack()
-    text_box.pack()
+    #Term entry frame
+    frm_entry = tk.Frame(master=window)
+    ent_number = tk.Entry(master=frm_entry, width=10)
 
+
+    btn_submit = tk.Button(
+        text="Submit",
+        width=2,
+        height=1,
+        bg="black",
+        fg="white",
+    )
+    btn_submit.grid(
+        row=3,
+        column=0,
+    )
+
+    btn_submit.pack()
+    ent_number.pack()
     window.mainloop()
+
